8d0512d6e4ec942c989a9fb4a17a0c06a19024b5
nichcream <c.potato23@gmail.com>
2017-11-09 15:08:09 +0000

    net: ping: Fix compilation errors

--- net/ipv4/ping.c
+++ net/ipv4/ping.c
@@ -841,13 +841,10 @@ int ping_recvmsg(struct kiocb *iocb, struct sock *sk, struct msghdr *msg,
 			return ip_recv_error(sk, msg, len, addr_len);
 #if IS_ENABLED(CONFIG_IPV6)
 		} else if (family == AF_INET6) {
-			return pingv6_ops.ipv6_recv_error(sk, msg, len);
+			return pingv6_ops.ipv6_recv_error(sk, msg, len, addr_len);
 #endif
 		}
 	}
-=======
-	if (flags & MSG_ERRQUEUE)
-		return ip_recv_error(sk, msg, len, addr_len);
 
 	skb = skb_recv_datagram(sk, flags, noblock, &err);
 	if (!skb)
